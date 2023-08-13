fe781c17b4d4b95d2c50b480596c73d8ae9bc181
md <mdmonish143@gmail.com>
2023-08-13 18:45:01 +0530

    login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,10 +7,10 @@ function Login({ isLogIn, setLogIn,setUser }) {
 
   const dispatch = useDispatch();
   const users = useSelector((state) => state.user.users);
-  console.log("users", users);
   // const [users, setUsers] = useState([]);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error,setError] = useState(false)
   // const fetchUser = async () => {
   //     const base = "https://fakestoreapi.com/users";
   //     const response = await fetch(base);
@@ -18,20 +18,21 @@ function Login({ isLogIn, setLogIn,setUser }) {
   //     return data;
   //   };
   const checkUser = () => {
+    console.log(email,password)
     const usercheck = users.find(
       (user) => user.email === email && user.password === password
     );
     if (!email || !password) {
-      alert("Invalid details");
+      setError(true);
       return;
     }
     if (usercheck) {
-      console.log("Login successful");
+      setError(false)
       setLogIn(true);
       setUser(usercheck.username)
       history("/cart");
     } else {
-      console.log("Wrong password or username");
+      setError(true);
     }
   };
   const logout = () => {
@@ -58,7 +59,9 @@ function Login({ isLogIn, setLogIn,setUser }) {
       ) : (
         <div className="login__container">
           <h2>Login Page</h2>
-          <div><input
+          <div>
+          {error && <div style={{color:"red",display:"flex"}}>*Please provide correct credentials.</div>}
+          <input
             style={{ padding: 6,margin: "20px 0" }}
             type="email"
             placeholder="email"
@@ -74,9 +77,13 @@ function Login({ isLogIn, setLogIn,setUser }) {
               setPassword(e.target.value);
             }}
           /></div>
-          <div style={{display:"flex"}}> <button style={{flexGrow:0.6,padding: "5px 27px",marginTop:10}}onClick={checkUser}>Login</button></div>
-        </div>
+          <div style={{display:"flex",marginBottom:"200px",justifyContent:"center"}}> <button style={{flexGrow:0.6,padding: "5px 27px",marginTop:10}}onClick={checkUser}>Login</button></div>
+          <p>Disclaimer* email: john@gmail.com,
+password: m38rmF$, </p></div>
+        
       )}
+
+      
     </>
   );
 }
